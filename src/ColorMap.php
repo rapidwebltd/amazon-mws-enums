287f80c75fae70f3acd9fc656625bad98a79f402
Jordan Hall <jordan.hall@rapidweb.biz>
2017-09-08 11:04:48 +0100

    Added more options to `ColorMap` and `SizeMap`

--- src/ColorMap.php
+++ src/ColorMap.php
@@ -4,6 +4,19 @@ namespace RapidWeb\AmazonMwsEnums;
 
 abstract class ColorMap
 {
-    const GREEN = 'Green';
     const BLACK = 'Black';
+    const BLUE = 'Blue';
+    const BROWN = 'Brown';
+    const GOLD = 'Gold';
+    const GREEN = 'Green';
+    const GREY = 'Grey';
+    const GRAY = 'Grey';
+    const ORANGE = 'Orange';
+    const PINK = 'Pink';
+    const PURPLE = 'Purple';
+    const RED = 'Red';
+    const VIOLET = 'Violet';
+    const WHITE = 'White';
+    const YELLOW = 'Yellow';
+    const MULTICOLOURED = 'Multicoloured';
 }
--- src/SizeMap.php
+++ src/SizeMap.php
@@ -4,9 +4,16 @@ namespace RapidWeb\AmazonMwsEnums;
 
 abstract class SizeMap
 {
+    const ONE_SIZE = 'One size';
     const SMALL = 'Small';
+    const SMALL_MEDIUM = 'Small-Medium';
     const MEDIUM = 'Medium';
     const LARGE = 'Large';
     const XLARGE = 'X-Large';
     const XXLARGE = 'XX-Large';
+    const XXXLARGE = 'XXX-Large';
+    const XXXXLARGE = 'XXXX-Large';
+    const XXXXXLARGE = 'XXXXX-Large';
+    const XXSMALL = 'XX-Small';
+    const XSMALL = 'X-Small';
 }
